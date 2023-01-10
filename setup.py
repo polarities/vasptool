a73bfb815bb651ffd701c845d670623f3635d5fb
Tuna Lee <10000685+polarities@users.noreply.github.com>
2023-01-10 23:30:51 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vasptool',
+    version='',
+    packages=['vasptool'],
+    url='',
+    license='',
+    author='Jordy Lee',
+    author_email='',
+    description=''
+)
---
+++ vasptool/__init__.py
@@ -0,0 +1,1 @@
+from _incar import INCAR
